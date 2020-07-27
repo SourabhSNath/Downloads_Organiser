0f59be562058a2d62b27c74803c818df5d58f781
Sourabh S Nath <sourabhsnath@gmail.com>
2020-07-27 18:11:42 +0530

    Fixed bug where the extensions split up

--- file_extensions.py
+++ file_extensions.py
@@ -17,16 +17,22 @@ EXTENSIONS = {
     "ARCHIVES": (".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
                  ".dmg", ".rar", ".xar", ".zip"),
     "AUDIO": (".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p",
-              ".mp3",
+              ".mp3", ".md",
               ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"),
     "PLAINTEXT": (".txt", ".in", ".out"),
-    "PDF": ".pdf",
-    "EXE": ".exe"
+    "Shell": (".sh", ".fish", ".zsh", ".ps1", ".csh"),
+    # Needs to be a tuple with empty string, else For loop seems to be breaking the string into pieces
+    # like this {".": "OS"}, {"d": "OS"}, {"e": OS} inside file_formats
+    "OS": (".deb", ""),
+    "PDF": (".pdf", ""),
+    "EXE": (".exe", ""),
+    "Folders": (""),
+    "Other-Files": (".", "")
 }
 
 # Seperate all the extensions
 file_formats = {
     file: directory
-    for directory, file_formats in EXTENSIONS.items()
-    for file in file_formats
+    for directory, file_extensions in EXTENSIONS.items()
+    for file in file_extensions
 }
